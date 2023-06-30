5a4bf80c79812769e3b198d2abb9a65f6f8d6aeb
WANWISAAA <chumpornw6@gmail.com>
2023-06-30 15:46:02 -0800

    first app

---
+++ bin/dart_if_else_if.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main(){
+  int score;
+
+  print("Enter Score: ");
+  score = int.parse(stdin.readLineSync()!);
+
+  if((score>=80) & (score<=100)){
+    print("Score = $score : GRADE A");
+  }
+  else if((score>=75) & (score<=79)){
+    print("Score = $score : GRADE B+");
+  }
+  else if((score>=70) & (score<=79)){
+    print("Score = $score : GRADE B");
+  }
+  else if((score>=65) & (score<=69)){
+    print("Score = $score : GRADE C+");
+  }
+  else if((score>=60) & (score<=69)){
+    print("Score = $score : GRADE C");
+  }
+  else if((score>=55) & (score<=59)){
+    print("Score = $score : GRADE D+");
+  }
+  else if((score>=50) & (score<=59)){
+    print("Score = $score : GRADE D");
+  }
+  else if((score>=0) & (score<=49)){
+    print("Score = $score : GRADE F");
+  }
+  else{print("ERROR: Value not found");
+  }
+}
